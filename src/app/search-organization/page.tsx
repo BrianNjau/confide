40a82c02a4a32578cbfec0c46bd5217f91440d73
Brian <brian.wahinya.njau@huawei.com>
2024-04-25 15:38:51 +0300

    Introduce suspense on Search Organization

--- src/app/search-organization/page.tsx
+++ src/app/search-organization/page.tsx
@@ -2,6 +2,7 @@ import PageHeader from "@/app/shared/page-header";
 import ProductsGrid from "@/app/shared/explore-listing";
 import ListingFilters from "@/app/shared/explore-listing/listing-filters";
 import { metaObject } from "@/config/site.config";
+import { Suspense } from "react";
 
 export const metadata = {
   ...metaObject("Search For Your Organization"),
@@ -24,7 +25,10 @@ export default function SearchOrganizationPage() {
   return (
     <div className="@container m-16">
       <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb} />
-      <ListingFilters className="mb-6" />
+      <Suspense>
+        <ListingFilters className="mb-6" />
+      </Suspense>
+
       <ProductsGrid />
     </div>
   );
